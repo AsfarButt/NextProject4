62e6d76724075d8bbcc9701761812167b667e9f2
AsfarButt <asfarbutt69@gmail.com>
2025-10-29 19:28:25 +0500

    Update 5

--- app/text.tsx
+++ app/text.tsx
@@ -3,14 +3,29 @@ import React,{useRef, useState, useEffect} from "react";
 
 
 
-
-
 export default function Text({activeindex}:{activeindex: number}){
+
+    const [Name, setName] = useState([]);
+
+    async function GetData(){
+        const data = await fetch("/smallimage.json").then((res) => res.json());
+        const letter = data[activeindex][1].split(" ");
+        setName(letter);
+        console.log("Data: ",data);
+    }
+
+    useEffect(() => {
+        GetData();
+    },[])
+
+    useEffect(() => {
+        GetData();
+    },[activeindex])
 
 
 
     return(<div className="relative w-full h-full bg-black/10">
-            <div className="w-full h-[50%] border-box bg-green-600/20">Corn</div>
-            <div className="w-full h-[50%] border-box bg-red-500/20">Soup</div>
+            <div className="w-full h-[50%] border-box bg-green-600/20 text-2xl font-semibold"><h1 className="h-full">{Name[0]}</h1></div>
+            <div className="w-full h-[50%] border-box bg-red-500/20 text-2xl font-semibold"><h1 className="h-full">{Name[1]}</h1></div>
     </div>)
 }
